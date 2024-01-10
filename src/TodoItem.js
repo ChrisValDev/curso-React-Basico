d9f7137dd7228c0994e0e05ddd0d35fb9b5aa5e7
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-01-10 14:16:32 -0600

    Agregando colores a Iconos SVG y funcionalidad para completar y eliminar

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,7 +5,10 @@ import './TodoItem.css';
 function TodoItem(props) {
     return (
       <li className="TodoItem">
-        <CompleteIcon />
+        <CompleteIcon
+            completed={props.completed}
+            onComplete={props.onComplete}
+        />
         {/* <span 
             className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
             onClick={props.onComplete}
@@ -15,7 +18,9 @@ function TodoItem(props) {
         <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
             {props.text}
         </p>
-        <DeleteIcon />
+        <DeleteIcon
+            onDelete={props.onDelete}
+        />
         {/* <span 
             className="Icon Icon-delete"
             onClick={props.onDelete}
