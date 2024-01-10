4c34faf1a44dcb2914419aae57f170ef410f3088
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-01-10 01:37:50 -0600

    Eventos en React: onClick y onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -5,6 +5,14 @@ function TodoSearch() {
         <input 
             placeholder="Cortar cebolla"
             className="TodoSearch"
+            onChange={
+                (event) => {
+                    console.log('Escribiste en el TodoSearch');
+                    console.log(event);
+                    console.log(event.target);
+                    console.log(event.target.value);
+                }
+            }
         />
     );
 }
