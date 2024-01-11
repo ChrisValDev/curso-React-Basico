255a41352a94cc8d450048cbad8dcc6725eaf3e2
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-01-11 16:33:35 -0600

    Organizando modulos de mejor manera

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
+import { AppUI } from './AppUI';
 import { useLocalStorage } from './useLocalStorage';
 
 // Esto comentado se ingresa en consola a LocalStorage para el ejemplo
@@ -51,30 +47,15 @@ function App() {
     };
 
     return (
-        <>
-            <TodoCounter 
-                completed={completedTodos}
-                total={totalTodos}
-            />
-            <TodoSearch
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-            />
-
-            <TodoList>
-                {searchedTodos.map(todo => (
-                    <TodoItem 
-                        key={todo.text}
-                        text={todo.text}
-                        completed={todo.completed}
-                        onComplete={() => makeTodo(todo.text)}   
-                        onDelete={() => deleteTodo(todo.text)}   
-                    />
-                ))}
-            </TodoList>
-
-            <CreateTodoButton />
-        </>
+        <AppUI 
+            completedTodos={completedTodos}
+            totalTodos={totalTodos}
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            searchedTodos={searchedTodos}
+            makeTodo={makeTodo}
+            deleteTodo={deleteTodo}
+        />
     );
 };
 
