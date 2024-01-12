28ad1f2e88a46380b2f7f5c776c906ddfb890e83
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-01-11 19:34:18 -0600

    Actualizando estados con useEffect

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,6 +6,8 @@ import { CreateTodoButton } from '../CreateTodoButton';
 
 
 function AppUI({
+    loading,
+    error,
     completedTodos,
     totalTodos,
     searchValue,
@@ -26,6 +28,11 @@ function AppUI({
             />
 
             <TodoList>
+                {loading && <p>Estamos cargando</p>}
+                {error && <p>Hubo un error inesperado</p>}
+                {(!loading && searchedTodos.length == 0) && <p>Crea tu primer TODOList!</p>}
+                
+
                 {searchedTodos.map(todo => (
                     <TodoItem 
                         key={todo.text}
--- src/App/index.js
+++ src/App/index.js
@@ -15,7 +15,12 @@ import { useLocalStorage } from './useLocalStorage';
 // localStorage.setItem('TODOS_V1', stringifiedTodos);
 
 function App() {
-    const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+    } = useLocalStorage('TODOS_V1', []);
     const [searchValue, setSearchValue] = React.useState('');
 
     const completedTodos = todos.filter(todo => todo.completed === true).length;
@@ -47,7 +52,9 @@ function App() {
     };
 
     return (
-        <AppUI 
+        <AppUI
+            loading={loading}
+            error={error}
             completedTodos={completedTodos}
             totalTodos={totalTodos}
             searchValue={searchValue}
