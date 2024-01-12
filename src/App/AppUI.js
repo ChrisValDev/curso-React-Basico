28a75650912a2ff7abed45192fbfb754ecff00d1
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-01-12 14:30:59 -0600

    Ajustando los componentes para consumir props useContext

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -9,44 +10,41 @@ import { CreateTodoButton } from '../CreateTodoButton';
 import { TodoContext } from '../TodoContext';
 
 function AppUI() {
+    const {
+        loading,
+        error,
+        searchedTodos,
+        makeTodo,
+        deleteTodo,
+    } = React.useContext(TodoContext);
     return (
         <>
             <TodoCounter />
             <TodoSearch />
 
-            <TodoContext.Consumer>
-                {({
-                    loading,
-                    error,
-                    searchedTodos,
-                    makeTodo,
-                    deleteTodo,
-                }) => (
-                    <TodoList>
-                        {loading && (
-                            <>
-                            <TodosLoading />
-                            <TodosLoading />
-                            <TodosLoading />
-                            </>
-                        )}
-                        {error && <TodosError />}
-                        {(!loading && searchedTodos.length == 0) && <EmptyTodos />}
-                        
-
-                        {searchedTodos.map(todo => (
-                            <TodoItem 
-                                key={todo.text}
-                                text={todo.text}
-                                completed={todo.completed}
-                                onComplete={() => makeTodo(todo.text)}   
-                                onDelete={() => deleteTodo(todo.text)}   
-                            />
-                        ))}
-                    </TodoList>
+            <TodoList>
+                {loading && (
+                    <>
+                    <TodosLoading />
+                    <TodosLoading />
+                    <TodosLoading />
+                    </>
                 )}
-            </TodoContext.Consumer>
+                {error && <TodosError />}
+                {(!loading && searchedTodos.length == 0) && <EmptyTodos />}
+                
 
+                {searchedTodos.map(todo => (
+                    <TodoItem 
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => makeTodo(todo.text)}   
+                        onDelete={() => deleteTodo(todo.text)}   
+                    />
+                ))}
+            </TodoList>
+                
             <CreateTodoButton />
         </>
     );
